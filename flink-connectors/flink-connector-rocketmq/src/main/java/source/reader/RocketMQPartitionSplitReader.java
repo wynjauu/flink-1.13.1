400996fee19d6b3c51df7d1160e575946ebdd343
武阳 <wuyang09@100.me>
2021-09-27 15:19:37 +0800

    支持rocketmq connector

---
+++ flink-connectors/flink-connector-rocketmq/src/main/java/source/reader/RocketMQPartitionSplitReader.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright © 2018-2021 me.100.
+ *
+ * This file is part of flink-parent project.
+ * It can not be copied and/or distributed without the express
+ * permission of bigdata group.
+ */
+package source.reader;
+
+/**
+ * flink-parent: RocketMQPartitionSplitReader
+ *
+ * @author wuyang
+ * @version 1.2.0, 2021-09-25 23:02
+ * @since 1.2.0, 2021-09-25 23:02
+ */
+public class RocketMQPartitionSplitReader {
+}
---
+++ flink-connectors/flink-connector-rocketmq/src/main/java/source/reader/deserializer/DirtyDataStrategy.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright © 2018-2021 me.100.
+ *
+ * This file is part of flink-parent project.
+ * It can not be copied and/or distributed without the express
+ * permission of bigdata group.
+ */
+package source.reader.deserializer;
+
+/**
+ * flink-parent: DirtyDataStrategy
+ *
+ * @author wuyang
+ * @version 1.2.0, 2021-09-25 23:05
+ * @since 1.2.0, 2021-09-25 23:05
+ */
+public class DirtyDataStrategy {
+}
